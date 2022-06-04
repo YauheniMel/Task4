975218c322856d6d60d8aa41337e6c904b29d7ed
YauheniMel <207melnik@gmail.com>
2022-06-04 18:14:42 +0300

    Deploy

--- src/api/Routes.ts
+++ src/api/Routes.ts
@@ -1,11 +1,11 @@
 enum Routes {
-  login = 'https://dd-task-4.herokuapp.com/api/login',
-  register = 'https://dd-task-4.herokuapp.com/api/register',
-  getUsers = 'https://dd-task-4.herokuapp.com/api/users',
-  block = 'https://dd-task-4.herokuapp.com/api/block/',
-  unblock = 'https://dd-task-4.herokuapp.com/api/unblock/',
-  delete = 'https://dd-task-4.herokuapp.com/api/del/',
-  logout = 'https://dd-task-4.herokuapp.com/api/logout',
+  login = '/api/login',
+  register = '/api/register',
+  getUsers = '/api/users',
+  block = '/api/block/',
+  unblock = '/api/unblock/',
+  delete = '/api/del/',
+  logout = '/api/logout',
 }
 
 export default Routes;
